e78dd56c7856bcb319cf494f60ff4127cc065017
1144kevin <s111019009@stu.ntue.edu.tw>
2024-01-02 13:18:43 +0800

    add potion

---
+++ Assets/Scripts/General/GateController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject Gate;
+
+    void Start()
+    {
+        // 获取Animator组件
+        animator = GetComponent<Animator>();
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "hit")
+        {
+            animator.SetInteger("close", 1);
+
+            StartCoroutine(WaitForAnimation());
+        }
+    }
+    IEnumerator WaitForAnimation()
+    {
+        yield return new WaitForSeconds(0.6f);
+        Destroy(Gate);
+    }
+}
